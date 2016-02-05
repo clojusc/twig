d18f608f9609aa241508f50ff3f97aa502585463
Duncan McGreggor <oubiwann@gmail.com>
2016-02-04 18:52:45 -0600

    Added code I've used in other projects from last year.

--- project.clj
+++ project.clj
@@ -1,6 +1,28 @@
 (defproject twig "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A little logging helper for Clojure projects"
+  :url "https://github.com/oubiwann/twig"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]])
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/tools.logging "0.3.1"]
+                 [ch.qos.logback/logback-classic "1.1.3"]]
+  ;; List the namespaces whose log levels we want to control; note that if we
+  ;; add more dependencies that are chatty in the logs, we'll want to add them
+  ;; here.
+
+  ; :logging-namespaces [lcmap-rest
+  ;                      lcmap-client
+  ;                      com.datastax.driver
+  ;                      co.paralleluniverse]
+
+  ;; In you profiles, add an :env key and in :env, add a :log-level
+
+  ; :profiles {
+  ;   :dev {
+  ;     :env
+  ;       {:db {...}
+  ;        :http {...}
+  ;        ...
+  ;        :log-level :debug
+  ;        ...}}}
+  )
--- src/twig/core.clj
+++ src/twig/core.clj
@@ -1,6 +1,50 @@
-(ns twig.core)
+(ns twig.core
+  (:require [clojure.tools.logging :as log]
+            [clojure.tools.logging.impl :as log-impl]
+            [clojure.core.match :refer [match]])
+  (:import [ch.qos.logback.classic Level]
+           [ch.qos.logback.classic.joran JoranConfigurator]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn get-factory []
+  (log-impl/find-factory))
+
+(defn get-factory-name []
+  (log-impl/name (get-factory)))
+
+(defn get-logger [namespace]
+  (log-impl/get-logger (get-factory) namespace))
+
+(defn get-logger-name [namespace]
+  (.getName (get-logger namespace)))
+
+(defn get-logger-level [namespace]
+  (.getLevel (get-logger namespace)))
+
+(defn get-logger-context [namespace]
+  (.getLoggerContext (get-logger namespace)))
+
+(defn get-config [namespace]
+  (let [cfg (JoranConfigurator.)]
+    (.setContext cfg (get-logger-context namespace))
+    cfg))
+
+(defn convert-level [level]
+  (match [level]
+    [:off] Level/OFF
+    ;;[:fatal] Level/FATAL
+    [:error] Level/ERROR
+    [:warn] Level/WARN
+    [:info] Level/INFO
+    [:debug] Level/DEBUG
+    [:trace] Level/TRACE
+    [:all] Level/ALL))
+
+(defmulti set-level! (fn [namesp level] (mapv class [namesp level])))
+
+(defmethod set-level! [clojure.lang.Symbol clojure.lang.Keyword]
+                      [namesp level]
+  (.setLevel (get-logger namesp) (convert-level level)))
+
+(defmethod set-level! [clojure.lang.PersistentVector clojure.lang.Keyword]
+                      [namesps level]
+  (dorun (map #(set-level! % level) namesps)))
