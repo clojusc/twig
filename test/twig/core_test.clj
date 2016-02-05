5fbefaad1c86b9fea80515f8eaf04c199ea16d67
Duncan McGreggor <oubiwann@gmail.com>
2016-02-04 19:03:20 -0600

    Added unit tests for convert-level.

--- test/twig/core_test.clj
+++ test/twig/core_test.clj
@@ -1,7 +1,13 @@
 (ns twig.core-test
   (:require [clojure.test :refer :all]
-            [twig.core :refer :all]))
+            [twig.core :as twig])
+  (:import [ch.qos.logback.classic Level]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest convert-level-test
+  (is (= (twig/convert-level :off) Level/OFF))
+  (is (= (twig/convert-level :error) Level/ERROR))
+  (is (= (twig/convert-level :warn) Level/WARN))
+  (is (= (twig/convert-level :info) Level/INFO))
+  (is (= (twig/convert-level :debug) Level/DEBUG))
+  (is (= (twig/convert-level :trace) Level/TRACE))
+  (is (= (twig/convert-level :all) Level/ALL)))
