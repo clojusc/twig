490eaa91eb6561d7fc920fc1e2c78f9ce5c7eef5
Duncan McGreggor <oubiwann@gmail.com>
2016-02-19 13:42:35 -0600

    Updated to use the latest version of logback.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject twig "0.1.4"
+(defproject twig "0.1.5"
   :description "A little logging helper for Clojure projects"
   :url "https://github.com/oubiwann/twig"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [ch.qos.logback/logback-classic "1.1.3"]])
+                 [ch.qos.logback/logback-classic "1.1.5"]])
