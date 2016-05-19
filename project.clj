d9b078b622b3fe28135ac5552301cb7565d8ee58
Duncan McGreggor <oubiwann@gmail.com>
2016-05-19 12:40:02 -0500

    Added dep.

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [ch.qos.logback/logback-classic "1.1.3"]])
+                 [ch.qos.logback/logback-classic "1.1.3"]
+                 [janino "2.5.10"]])
