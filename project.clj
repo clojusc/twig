da8e148e01ac34152ffe76bbecba650b01d76d20
Duncan McGreggor <oubiwann@gmail.com>
2016-08-16 00:11:32 -0500

    Bump version number.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojusc/twig "0.2.1"
+(defproject clojusc/twig "0.2.2"
   :description "A little logging helper for Clojure projects"
   :url "https://github.com/clojusc/twig"
   :license {:name "Eclipse Public License"
