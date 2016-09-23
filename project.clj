a9b7e563925749a9ba2c678fe7f5cd02ccbab46f
Duncan McGreggor <oubiwann@gmail.com>
2016-09-23 11:01:35 -0500

    Added exclusions to prevent multiple binding warnings.
    See: http://www.slf4j.org/codes.html#multiple_bindings

--- project.clj
+++ project.clj
@@ -5,5 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [ch.qos.logback/logback-classic "1.1.7"]
+                 [ch.qos.logback/logback-classic "1.1.7" :exclusions [
+                   org.slf4j/slf4j-log4j12
+                   log4j/log4j]]
                  [janino "2.5.15"]])
