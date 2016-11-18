27b762330a649be11aab8a1126fa8b3d8fba8fef
Duncan McGreggor <oubiwann@gmail.com>
2016-11-17 18:23:11 -0600

    Updated tests to use new function name.

--- test/clojusc/twig_test.clj
+++ test/clojusc/twig_test.clj
@@ -3,32 +3,42 @@
             [clojusc.twig :as twig])
   (:import [ch.qos.logback.classic Level]))
 
+(deftest keywordlevel->java-test
+  (is (= (twig/level->java :off) Level/OFF))
+  (is (= (twig/level->java :error) Level/ERROR))
+  (is (= (twig/level->java :warn) Level/WARN))
+  (is (= (twig/level->java :info) Level/INFO))
+  (is (= (twig/level->java :debug) Level/DEBUG))
+  (is (= (twig/level->java :trace) Level/TRACE))
+  (is (= (twig/level->java :all) Level/ALL)))
+
+(deftest symbollevel->java-test
+  (is (= (twig/level->java 'off) Level/OFF))
+  (is (= (twig/level->java 'error) Level/ERROR))
+  (is (= (twig/level->java 'warn) Level/WARN))
+  (is (= (twig/level->java 'info) Level/INFO))
+  (is (= (twig/level->java 'debug) Level/DEBUG))
+  (is (= (twig/level->java 'trace) Level/TRACE))
+  (is (= (twig/level->java 'all) Level/ALL)))
+
+(deftest stringlevel->java-test
+  (is (= (twig/level->java "off") Level/OFF))
+  (is (= (twig/level->java "error") Level/ERROR))
+  (is (= (twig/level->java "warn") Level/WARN))
+  (is (= (twig/level->java "info") Level/INFO))
+  (is (= (twig/level->java "debug") Level/DEBUG))
+  (is (= (twig/level->java "trace") Level/TRACE))
+  (is (= (twig/level->java "all") Level/ALL)))
+
 (deftest keyword->level-test
-  (is (= (twig/->level :off) Level/OFF))
-  (is (= (twig/->level :error) Level/ERROR))
-  (is (= (twig/->level :warn) Level/WARN))
-  (is (= (twig/->level :info) Level/INFO))
-  (is (= (twig/->level :debug) Level/DEBUG))
-  (is (= (twig/->level :trace) Level/TRACE))
-  (is (= (twig/->level :all) Level/ALL)))
+  (is (= (twig/->level :off) "OFF"))
+  (is (= (twig/->level :error) "ERROR"))
+  (is (= (twig/->level :warn) "WARN"))
+  (is (= (twig/->level :info) "INFO"))
+  (is (= (twig/->level :debug) "DEBUG"))
+  (is (= (twig/->level :trace) "TRACE"))
+  (is (= (twig/->level :all) "ALL")))
 
-(deftest symbol->level-test
-  (is (= (twig/->level 'off) Level/OFF))
-  (is (= (twig/->level 'error) Level/ERROR))
-  (is (= (twig/->level 'warn) Level/WARN))
-  (is (= (twig/->level 'info) Level/INFO))
-  (is (= (twig/->level 'debug) Level/DEBUG))
-  (is (= (twig/->level 'trace) Level/TRACE))
-  (is (= (twig/->level 'all) Level/ALL)))
-
-(deftest string->level-test
-  (is (= (twig/->level "off") Level/OFF))
-  (is (= (twig/->level "error") Level/ERROR))
-  (is (= (twig/->level "warn") Level/WARN))
-  (is (= (twig/->level "info") Level/INFO))
-  (is (= (twig/->level "debug") Level/DEBUG))
-  (is (= (twig/->level "trace") Level/TRACE))
-  (is (= (twig/->level "all") Level/ALL)))
 
 ;; This next test is kept to test backwards compatibility with the old API
 (deftest convert-level-test
