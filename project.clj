eeff65559efe6aa4c95d239d762fc8b7058e3468
Duncan McGreggor <oubiwann@gmail.com>
2017-10-11 01:15:42 -0500

    Updated deps.

--- project.clj
+++ project.clj
@@ -3,24 +3,22 @@
   :url "https://github.com/clojusc/twig"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :exclusions [org.clojure/clojure]
   :dependencies [
-    [ch.qos.logback/logback-classic "1.2.3" :exclusions [
-      org.slf4j/slf4j-log4j12
-      org.slf4j/slf4j-api
-      log4j/log4j]]
+    [ch.qos.logback/logback-classic "1.2.3"]
     [clansi "1.0.0"]
     [clojusc/cljs-tools "0.2.0-SNAPSHOT"]
     [com.taoensso/timbre "4.10.0"]
     [org.clojure/clojure "1.8.0"]
-    [org.clojure/clojurescript "1.9.542"]
-    [org.clojure/tools.logging "0.3.1"]
+    [org.clojure/clojurescript "1.9.946"]
+    [org.clojure/tools.logging "0.4.0"]
     [org.slf4j/slf4j-api "1.7.25"]]
   :plugins [
-    [lein-cljsbuild "1.1.6" :exclusions [org.clojure/clojure]]
+    [lein-cljsbuild "1.1.7" :exclusions [org.clojure/clojure]]
     [lein-npm "0.6.2" :exclusions [org.clojure/clojure]]]
   :npm {
     :dependencies [
-      [[colors "1.1.2"]]}
+      [colors "1.1.2"]]}
   :cljsbuild {
     :builds [
       {:id "twig"
@@ -46,15 +44,17 @@
       ^{:doc "Start a browser-based Clojurescript REPL"}
       ["trampoline" "run" "-m" "clojure.main"
        "dev-resources/scripts/browser-repl.clj"]
-     }
+    "check-deps"
+      ^{:doc "Check for all old dependencies."}
+      ["with-profile" "+test" "ancient" "check" ":all"]}
   :profiles {
-    :uberjar {
+    :ubercompile {
       :aot :all}
     :test {
       :plugins [
-        [jonase/eastwood "0.2.3" :exclusions [org.clojure/clojure]]
-        [lein-kibit "0.1.5" :exclusions [org.clojure/clojure]]
-        [lein-ancient "0.6.10"]]}
+        [jonase/eastwood "0.2.4"]
+        [lein-kibit "0.1.5"]
+        [lein-ancient "0.6.12"]]}
     :dev {
       :source-paths ["dev-resources/src"]
       :repl-options {
