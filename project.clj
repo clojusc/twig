10ec7221ba38b4cd4ebaad7d5515c792865dd6b5
Duncan McGreggor <oubiwann@gmail.com>
2018-07-18 18:30:26 -0500

    Bumped for a point release.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojusc/twig "0.4.0-SNAPSHOT"
+(defproject clojusc/twig "0.3.3"
   :description "A little logging helper for Clojure projects"
   :url "https://github.com/clojusc/twig"
   :license {:name "Eclipse Public License"
