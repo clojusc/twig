e6ff7a60bb8f7fb9174e50336d3479fe555fa5c7
Duncan McGreggor <oubiwann@gmail.com>
2019-01-26 20:28:27 -0600

    Added cljs to the build.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojusc/twig "0.4.1-SNAPSHOT"
+(defproject clojusc/twig "0.4.1"
   :description "A little logging helper for Clojure/ClojureScript projects"
   :url "https://github.com/clojusc/twig"
   :license {
